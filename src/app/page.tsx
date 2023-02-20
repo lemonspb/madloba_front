d787ee14c405fd45cfb6be393ebf16aa58b849f8
Михаил <misha.skopenko@yandex.ru>
2023-02-20 19:42:13 +0400

    add mock data page product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { ListCategory } from "@/layouts/listCategory/listCategory";
 import ListProducts from "@/layouts/listProducts/listProducts";
 import { MainContainer } from "@/layouts/mainContainer/mainContainer";
+import { Roboto } from "@next/font/google";
 import styles from "./page.module.css";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
     <MainContainer>
-      <main className={styles.main}>
+      <main className={`${styles.main}`}>
         <ListCategory />
         <ListProducts />
       </main>
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Typography } from "antd";
+import { Typography, Descriptions } from "antd";
 import { MainContainer } from "@/layouts/mainContainer/mainContainer";
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 export default function Product() {
   return (
@@ -12,8 +12,34 @@ export default function Product() {
         <div></div>
         <div>
           <div>юзер</div>
-          <div>характеристики</div>
-          <div>Описание</div>
+          <div>
+            <Title level={3}>Характеристики</Title>
+            <Descriptions column={2}>
+              <Descriptions.Item label="Состояние">Новое</Descriptions.Item>
+              <Descriptions.Item label="Размер">80 х 80 см.</Descriptions.Item>
+              <Descriptions.Item label="Материал">Фотобумага</Descriptions.Item>
+              <Descriptions.Item label="Состояние">Новое</Descriptions.Item>
+              <Descriptions.Item label="Размер">80 х 80 см.</Descriptions.Item>
+              <Descriptions.Item label="Материал">Фотобумага</Descriptions.Item>
+            </Descriptions>
+          </div>
+          <div>
+            <Title level={3}>Описание</Title>
+            <Paragraph>
+              Это отличный подарок себе, друзьям, маме, бабушке, родственникам и
+              коллегам. Мы печатаем постеры на качественной глянцевой премиум
+              бумаге плотностью 200 гр/м. с нанесением качественного,
+              светостойкого, невыцветающего принта. Постеры для интерьера станут
+              ярким акцентом в доме или на даче и добавят еще больше уюта и
+              красоты. Набор постеров - прекрасное украшение для стен в спальне,
+              детской, прихожей, коридоре, гостиной или кухне. В комплекте: 3
+              постера. Все тщательно упаковано для сохранности во время
+              транспортировки. Добавляйте наш магазин в избранное и первыми
+              узнавайте о новинках и акциях! В нашем магазине большой выбор
+              товаров для дома: кружки, картины на холсте, стикеры, планеры,
+              значки и многое другое!
+            </Paragraph>
+          </div>
         </div>
       </div>
     </MainContainer>
